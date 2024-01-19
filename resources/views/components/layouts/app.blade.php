580f220d9a9da481f476c8afcc4e147d7837a235
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-19 15:28:53 -0300

    Lesson 2 - Building a static table

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Data Tables' }}</title>
+
+        {{-- @vite('resources/css/app.css') --}}
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    </head>
+    <body>
+        <main class="container-md flex items-center">
+
+            {{ $slot }}
+        </main>
+    </body>
+</html>
