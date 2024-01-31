037f6c1461b84c4239027833b22ef1d1400e7911
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-01-31 09:27:30 -0300

    Lesson 17 - Charting real data

--- app/Livewire/Order/Index/Chart.php
+++ app/Livewire/Order/Index/Chart.php
@@ -2,12 +2,39 @@
 
 namespace App\Livewire\Order\Index;
 
+use Illuminate\Support\Facades\DB;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Reactive;
 use Livewire\Component;
+use App\Models\Store;
 
 class Chart extends Component
 {
+    public Store $store;
+    public $dataset = [];
+
+    public function fillDataset()
+    {
+        $results = $this->store->orders()
+            ->select(
+                DB::raw("strftime('%Y', ordered_at) || '-' || strftime('%m', ordered_at) as increment"),
+                DB::raw('SUM(amount) as total'),
+            )
+            ->groupBy('increment')
+            ->get();
+
+        $this->dataset['values'] = $results->pluck('total')->toArray();
+        $this->dataset['labels'] = $results->pluck('increment')->toArray();
+    }
+
     public function render()
     {
+        $this->fillDataset();
         return view('livewire.order.index.chart');
+    }
+
+    public function placeholder()
+    {
+        return view('livewire.order.index.chart-placeholder');
     }
 }
